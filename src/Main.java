64105c8ab319f20c3b1d07ec9c7d1872afed9fef
shawt <shawtyhead@mail.ru>
2024-04-09 14:25:05 +0300

    course work v2.2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         employee[2] = new Employee("Novitskiy Konstantin Pavlovich", 3, 178000);
         employee[3] = new Employee("Aksyomov Svyatoslav Rodionovich", 2, 77000);
         employee[4] = new Employee("Boyko Svetlana Grigoryevna", 3, 110000);
+        employee[5] = new Employee("Alexandrov Petr Afanasyevich", 2, 219000);
+        employee[6] = new Employee("Lee Irina Sergeevna", 1, 133000);
     }
     //full array
     public static void getListOfEmployees() {
@@ -24,35 +26,43 @@ public class Main {
         return sum;
     }
     //max salary
-    public static double maxSalary() {
+    public static Employee employeeMaxSalary() {
+        Employee employeeMaxSalary = employee[0];
         double maxSalary = 0;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getSalary() > maxSalary) {
                 maxSalary = employee[i].getSalary();
+                employeeMaxSalary = employee[i];
             }
         }
-        return maxSalary;
+        return employeeMaxSalary;
     }
     //min salary
-    public static double minSalary() {
-        double minSalary = employee[0].getSalary();
+    public static Employee employeeMinSalary() {
+        Employee employeeMinSalary = employee[0];
+        double minSalary = Integer.MAX_VALUE;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getSalary() < minSalary) {
                 minSalary = employee[i].getSalary();
+                employeeMinSalary = employee[i];
             }
         }
-        return minSalary;
+        return employeeMinSalary;
     }
     //salary average
     public static double salaryAverage() {
         int sumEmployees = 0;
+        double salaryAverage = 0;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null) {
                 sumEmployees++;
             }
+            if (sumEmployees !=0) {
+                salaryAverage = Main.sum() / sumEmployees;
+            } else {
+                System.out.println("Кол-во сотрудников = 0");
+            }
         }
-        double salaryAverage = Main.sum() / sumEmployees;
-        System.out.println("Среднее значение зарплат = " + salaryAverage);
         return salaryAverage;
     }
     //employees' full names
@@ -65,45 +75,45 @@ public class Main {
     }
     //increase salary for all
     public static void increaseSalaryForAll() {
-        double percent = 5 / 100;
+        double percent = 5;
         double newSalary = 0;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null) {
-                newSalary = employee[i].getSalary() + employee[i].getSalary() * percent;
+                newSalary = employee[i].getSalary() + employee[i].getSalary() * percent / 100;
                 employee[i].setSalary(newSalary);
             }
         }
     }
     //by department
-    public static int department() {
-        int department = 1;
-        return department;
-    }
     //max salary
-    public static double maxSalaryByDep() {
-        int department = Main.department();
+    public static Employee maxSalaryByDep() {
+        Employee employeeMaxSalaryByDep = employee[0];
+        int department = 1;
         double maxSalary2 = 0;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getDepartment() == department && employee[i].getSalary() > maxSalary2) {
                 maxSalary2 = employee[i].getSalary();
+                employeeMaxSalaryByDep = employee[i];
             }
         }
-        return maxSalary2;
+        return employeeMaxSalaryByDep;
     }
     //min salary
-    public static double minSalaryByDep() {
-        int department = Main.department();
-        double minSalary2 = Main.maxSalaryByDep();
+    public static Employee minSalaryByDep() {
+        Employee employeeMinSalaryByDep = employee[0];
+        int department = 1;
+        double minSalary2 = Integer.MAX_VALUE;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getDepartment() == department && employee[i].getSalary() < minSalary2) {
                 minSalary2 = employee[i].getSalary();
+                employeeMinSalaryByDep = employee[i];
             }
         }
-        return minSalary2;
+        return employeeMinSalaryByDep;
     }
     //sum
     public static double sumByDep() {
-        int department = Main.department();
+        int department = 1;
         double sum2 = 0;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getDepartment() == department) {
@@ -115,25 +125,30 @@ public class Main {
 
     //salary average
     public static double salaryAverageByDep() {
-        int department = Main.department();
+        int department = 1;
         int sumEmployees2 = 0;
+        double salaryAverage2 = 0;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getDepartment() == department) {
                 sumEmployees2++;
             }
+            if (sumEmployees2 != 0) {
+                salaryAverage2 = Main.sumByDep() / sumEmployees2;
+            } else {
+                System.out.println("Кол-во сотрудников = 0");
+            }
         }
-        double salaryAverage2 = Main.sumByDep() / sumEmployees2;
         return salaryAverage2;
     }
 
     //increase salary for department
     public static void increaseSalaryByDep() {
-        int department = Main.department();
-        double percent2 = 10 / 100;
+        int department = 1;
+        double percent2 = 10;
         double newSalary2 = 0;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getDepartment() == department) {
-                newSalary2 = employee[i].getSalary() + employee[i].getSalary() * percent2;
+                newSalary2 = employee[i].getSalary() + employee[i].getSalary() * percent2 / 100;
                 employee[i].setSalary(newSalary2);
             }
         }
@@ -141,7 +156,7 @@ public class Main {
 
     //array department
     public static void getEmpleyeesByDep() {
-        int department = Main.department();
+        int department = 1;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getDepartment() == department) {
                 System.out.println(employee[i].toStringWithoutDepartment());
@@ -150,14 +165,9 @@ public class Main {
     }
 
     //by parameter
-    public static double setParameter() {
-        double givenParameter = 100000;
-        return givenParameter;
-    }
-
     //less than
     public static void lessThanParameter() {
-        double givenParameter = Main.setParameter();
+        double givenParameter = 100000;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getSalary() < givenParameter) {
                 System.out.println(employee[i].toStringWithoutDepartment());
@@ -167,7 +177,7 @@ public class Main {
 
     //more than
     public static void moreThanParameter() {
-        double givenParameter = Main.setParameter();
+        double givenParameter = 100000;
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] != null && employee[i].getSalary() > givenParameter) {
                 System.out.println(employee[i].toStringWithoutDepartment());
